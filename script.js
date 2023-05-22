93631c4c737b68575f6d89c128eaf70ca6922d81
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-22 16:35:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+ var fname = document.getElementsByTagName("input")[0].value;
+ var lname = document.getElementsByTagName("input")[1].value;
+	alert(`${fname} ${lname}`)
+	
 }
